1f50dc89948ad97870b177d914b34f0e45078b79
gcontolini <gina@contolini.com>
2026-07-14 13:36:51 -0700

    final amend from bash

--- git_command_notes.R
+++ git_command_notes.R
@@ -3,7 +3,7 @@
 git remote add origin https://github.com/gcontolini/GFA_Kelp_Restoration.git
 # open the directory locally
 cd ~/Desktop/GFA_Kelp_Restoration
-# pull from remote
+# pull from remote 
 git pull 
 # see what files are being tracked
 git ls-files 
@@ -12,9 +12,9 @@ git status
 # shows if you are working on a branch or if you're up to date with main.
 # do some work, then add something you worked on
 git add filename.Rmd
-# commit what you just added
+# commit what you just added 
 git commit -m "message"
-# amend a commit since you're not done with the thing you're working on yet
+# amend a commit since you're not done with the thing you're working on yet 
 git add filename.Rmd
 git commit --amend --no-edit
 # make the final commit after you finished amending a bunch of times
